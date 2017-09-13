57637dc72c93ddd1e1d07b76bb37d9e26d1137b0
anusdemolition <31025645+anusdemolition@users.noreply.github.com>
2017-09-12 19:08:00 -0500

    b

---
+++ angb.js
@@ -0,0 +1,23 @@
+var app = angular.module('nsApp', []);
+
+app.filter("trustUrl", ['$sce', function($sce) {
+  return function(recordingUrl) {
+    return $sce.trustAsResourceUrl(recordingUrl);
+  };
+}]);
+
+app.controller('controllerIndex', function($scope) {
+  $scope.videoLink = null;
+  $scope.showVideo = false;
+
+  $scope.getVideo = function() {
+    let input = $scope.bLink;
+    if (input != null && input != '') {
+      let tagReg = input.substring(0, input.lastIndexOf('&apiKey=undefined'));
+      let link = `tagReg&apiKey=${NDRkOTk3NzA2YTdjZjc1ZGIxODJmMTMyNWU5MDYxYzA2NDkyMTgxYQ==}`;
+      console.log(link);
+      $scope.videoLink = link;
+      $scope.showVideo = true;
+    }
+  };
+});
